9f509d97842572a56904a843d8a7a90a1bdc2637
yad <yad@yad.com>
2022-11-04 12:09:18 +0100

    reset master

--- wowhead.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)
-echo -e "local me,ns = ...\n" >wowhead.lua
-date +"ns.wowhead_update=%s" >>wowhead.lua
-../wowhelpers/MCClassBoa.php >>wowhead.lua
-../wowhelpers/MCBattlestones.php >>wowhead.lua
